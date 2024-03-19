f98e2bd42c04dd84f1c0c67b7c131eba111cfdb9
CaxaROCK <yakubueric@gmail.com>
2024-03-19 11:02:23 +0400

    fix: tabs switch

--- script_custom.js
+++ script_custom.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector(".lc-header-lpc__fixed-wrapper");
+  // burger
   const burgerLink = document.querySelectorAll(
     ".lc-navigation-menu__item_layout_vertical"
   );
@@ -14,13 +15,19 @@ document.addEventListener("DOMContentLoaded", () => {
     ".lc-header-lpc-burger__menu_align_right"
   );
 
+  // tabs
   const tabs = document.querySelectorAll(".lc-tabs__tab_theme_textWithIcon");
+  const tabsContents = document.querySelectorAll(".lc-tabs__content");
+  const tabItems = document.querySelectorAll(".lc-features__items");
 
+  // acordion
   const faqHeads = document.querySelectorAll(".lc-spoiler-item__header");
   const faqTexts = document.querySelectorAll(".lc-spoiler-item");
 
   faqTexts.forEach((text) => text.classList.remove("lc-spoiler-item_open"));
-
+  tabItems.forEach((item, index) => {
+    if (index < 3) item.classList.add(`active-tabs-${index + 1}`);
+  });
   // events
 
   tabs.forEach((tab) => tab.addEventListener("click", toggleTab));
@@ -58,6 +65,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const tabClicked = e.target.closest(".lc-tabs__tab_theme_textWithIcon");
 
     if (!tabClicked) return;
+
+    const index = Array.from(tabs).indexOf(tabClicked);
+
+    tabsContents.forEach((content) => {
+      content.classList.remove("lc-tabs__content_visible");
+    });
+
+    tabsContents[index].classList.add("lc-tabs__content_visible");
 
     tabs.forEach((tab) => {
       tab.style.border = "3px solid rgba(40, 40, 40, 0)";
